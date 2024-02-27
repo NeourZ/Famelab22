4950a2b4a74a1bb6ff44b8203556491feed41ab2
Fame <pattanitfame@gmail.com>
2024-02-27 14:18:01 +0700

    completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main(int numbers, char *arv[])
+{
+    float sum = 0;
+    if (numbers == 1)  cout << "Please input numbers to find average." << endl;
+    else
+    {
+        for (int i = 0; i < numbers - 1; i++)
+        {
+            sum += atof(arv[i + 1]);
+        }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << numbers - 1 << " numbers = " << sum  / double(numbers - 1) << endl;
+        cout << "---------------------------------" << endl;
+    }
+}
